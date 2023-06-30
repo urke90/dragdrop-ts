e2259b651d483ab7ac9964502fbdce9ed7d217ac
urke90 <you@example.com>
2023-06-30 11:03:58 +0200

    added aoutbind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,67 @@
+// autobind decorators
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjustedDescriptor;
+}
+
+// Main Project Class
 class Project {
     templateEl: HTMLTemplateElement;
     hostEl: HTMLDivElement;
     formElement: HTMLFormElement;
+    titleInput: HTMLInputElement;
+    descriptionInput: HTMLInputElement;
+    peopleInput: HTMLInputElement;
+    submitButton: HTMLButtonElement;
 
-    constructor(){
-        this.templateEl = document.getElementById("project-input")! as HTMLTemplateElement;
-        this.hostEl = <HTMLDivElement>document.getElementById("app")!
+    constructor() {
+        this.templateEl = document.getElementById(
+            'project-input'
+        )! as HTMLTemplateElement;
+        this.hostEl = <HTMLDivElement>document.getElementById('app')!;
 
         const importedNode = document.importNode(this.templateEl.content, true);
-        this.formElement =importedNode.firstElementChild as HTMLFormElement;
+        this.formElement = importedNode.firstElementChild as HTMLFormElement;
+        this.formElement.id = 'user-input';
+
+        this.titleInput = this.formElement.querySelector(
+            '#title'
+        )! as HTMLInputElement;
+
+        this.descriptionInput = this.formElement.querySelector(
+            '#description'
+        )! as HTMLInputElement;
+
+        this.peopleInput = this.formElement.querySelector(
+            '#people'
+        )! as HTMLInputElement;
+
+        this.submitButton = this.formElement.querySelector(
+            'button[type="submit"]'
+        )! as HTMLButtonElement;
+
+        this.configureListeners();
         this.attachForm();
+    }
+
+    @autobind
+    private handleSubmit(evt: Event) {
+        evt.preventDefault();
+        console.log('title', this.titleInput.value);
+    }
+
+    private configureListeners() {
+        this.formElement.addEventListener(
+            'submit',
+            this.handleSubmit.bind(this)
+        );
     }
 
     private attachForm() {
@@ -17,4 +69,4 @@ class Project {
     }
 }
 
-const newProject = new Project()
+const newProject = new Project();
